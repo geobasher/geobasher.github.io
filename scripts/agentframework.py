faea1cf8cf5789fa3416c80a54c290e436bc530a
geobasher <jackgatrell869@hotmail.com>
2018-10-14 19:08:58 +0100

    Update

--- scripts/agentframework.py
+++ scripts/agentframework.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 10 10:11:19 2018
+Created on Tus Oct 02 09:58:17 2018
+
+The purpose of this code is to model the agents behaviour within the environment
 
 @author: Jack Gatrell
-
-The purpose of this module is to define agent location, movementment and attributes.
 """
+
 
 import random
 
-
+#Creates the random location of each agent.
 class Agent:
     def __init__(self, environment, agents):
         self._x = random.randint(0,99) 
@@ -17,23 +18,28 @@ class Agent:
         self.environment = environment
         self.agents = agents
         self.store = 0     
-                   
+     
+              
     @property
     def getx(self):
         return self._x
+    
     
     @property
     def gety(self):
         return self._y
     
+    
     @getx.setter
     def setx(self, value):
         self.x = random.randint(0,99)
-        
+    
+    
     @gety.setter
     def sety(self, value):
         self.y = random.randint(0,99)
     
+    #Moves each agent around the environment randomly.
     def move(self):
         if random.random() <0.5:
             self._x = (self._x + 1) % 100
@@ -45,14 +51,17 @@ class Agent:
         else:
             self._y = (self._y - 1) % 100
     
-    def eat(self):# can you make it eat what is left?
-        if self.environment[self._y][self._x] > 0:
-            self.environment[self._y][self._x] -= 10
-            self.store += 10
-    
+    #Agent eats 10 from the environment.
+    def eat(self):
+            if self.environment[self._y][self._x] > 0:
+                self.environment[self._y][self._x] -= 10
+                self.store += 10
+     
+    #Calculates the distance between each agent.       
     def distance_between(self, agent):
         return (((self._x - agent._x)**2) + ((self._y - agent._y)**2))**0.5
     
+    #Shares store with closest agent.
     def share_with_neighbours(self, neighbourhood):
         for agent in self.agents:
             dist = self.distance_between(agent) 
@@ -61,5 +70,5 @@ class Agent:
                 ave = sum /2
                 self.store = ave
                 agent.store = ave
-                #print("sharing " + str(dist) + " " + str(ave))
+                
    
--- scripts/mainmodule.py
+++ scripts/mainmodule.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tuesday Oct 09 09:20:25 2018
+Created on Tus Oct 02 09:44:13 2018
+
+The purpose of this code is to model 25 agents within an environment and move 
+them randomly around based on the variable number. Once at a location the agent 
+will take 10 from the value of the environment.
+
+To run this program execute the code and it will display the agents and what they
+have ate.
 
 @author: Jack Gatrell
-
-The purpose of this module is to create a Agent Based Model to interacts with an
-environment.
 """
 
-
+#Importing relevent modules.
 import agentframework
 import csv
-import operator
 import random
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
-
 style.use('classic')
 
 
@@ -25,26 +27,21 @@ num_of_agents = 10
 num_of_iterations = 100
 neighbourhood = 20
 
+
 #Making the tables
 agents = []
 environment = []
 rowlist = []
+
 
 #Inputing the data in to the environment table.
 with open("in.txt") as f:
     reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
     for rowlist in reader:
         environment.append(rowlist)
-        
-def distance_between(agents_row_a, agents_row_b):
-    return (((agents_row_a.x - agents_row_b.x)**2) + 
-        ((agents_row_a.y - agents_row_b.y)**2))**0.5
 
-for agents_row_a in agents:
-    for agents_row_b in agents:
-        distance = distance_between(agents_row_a, agents_row_b)
 
-# Make the agents.
+#Ensure the agents are able to interact with the environment.
 for i in range(num_of_agents):
     agents.append(agentframework.Agent(environment, agents))
 
@@ -53,10 +50,20 @@ for i in range(num_of_agents):
 random.shuffle(agents)
 
 
+#Agents move and eat the environment avoiding each other.    
+for j in range(num_of_iterations):#How many time the agents will move.
+    for i in range(num_of_agents):#How many angents are prents in environment.
+        agents[i].move()#Moves the agent.
+        agents[i].eat()#Agent eats from environment.
+        agents[i].share_with_neighbours(neighbourhood)#Shares stores with other agents.
+
+       
+#Animation settions
 fig = plt.figure(figsize=(7, 7))
 ax = fig.add_axes([0, 0, 1, 1])
-#ax.set_autoscale_on(False)
 
+
+#Used to animate agents#
 def update(frame_number):
     
     fig.clear()
@@ -70,16 +77,7 @@ def update(frame_number):
             if random.random() < 0.5:
                 agents[i][1] = (agents[i][1] + 1) % 99 
             else:
-                agents[i][1] = (agents[i][1] - 1) % 99   
- 
-
-#Agents move and eat the environment avoiding each other.    
-for j in range(num_of_iterations):
-    for i in range(num_of_agents):
-        agents[i].move()
-        agents[i].eat()
-        agents[i].share_with_neighbours(neighbourhood)
-       
+                agents[i][1] = (agents[i][1] - 1) % 99  
 
        
 #Plot points on a graph.
@@ -89,7 +87,7 @@ plt.imshow(environment)
 for i in range(num_of_agents):
     plt.scatter(agents[i]._x, agents[i]._y)
     print(agents[i]._x, agents[i]._y)
-ani = animation.FuncAnimation(fig, update, interval=1000, repeat=False, frames=num_of_iterations)
+#ani = animation.FuncAnimation(fig, update, interval=1000, repeat=False, frames=num_of_iterations)#Could not get this to work.
 plt.show()
 
 
